8b8c4913b58eae7169d3d4e76581b1a94c55c5bc
ik-abaid <jaweryarahman@gmail.com>
2026-07-28 06:17:07 +0500

    Upload src/store.js

---
+++ src/store.js
@@ -0,0 +1,7 @@
+// src/store.js
+
+// In-memory cart storage (for demonstration purposes)
+// In a production environment, you would use a database like Redis or MongoDB.
+const carts = {};
+
+module.exports = { carts };
